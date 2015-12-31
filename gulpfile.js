85dbd22ff43972666218b48e5f75143b0a397805
pyanulis <pyanulis@gmail.com>
2015-12-31 04:17:24 +0300

    gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require("gulp"),
-    browserSync = require("browser-sync");
+    browserSync = require("browser-sync"),
+    jade = require('gulp-jade'),
+    plumber = require("gulp-plumber"),
+    compass = require('gulp-compass');
+
+var jadePath = './src/jade/*.jade';
+
+gulp.task('jade', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src(jadePath)
+        .pipe(plumber())
+        .pipe(jade({
+            locals: YOUR_LOCALS,
+            pretty: '\t'
+        }))
+        .pipe(gulp.dest('./app/'))
+});
 
 gulp.task("server", function(){
    browserSync({
@@ -10,12 +27,25 @@ gulp.task("server", function(){
    });
 });
 
+gulp.task('compass', function() {
+  gulp.src('./src/scss/main.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: './app/css',
+      sass: './src/scss'
+    }))
+});
+
 gulp.task("watch", function(){
    gulp.watch([
        "app/*.html",
        "app/js/**/*.js",
-       "app/css/**/*.css"
+       "app/css/**/*.*",
+       "app/html/**/*.*"
    ]).on("change", browserSync.reload);
+
+    gulp.watch(jadePath, ["jade"]);
+    gulp.watch("src/scss/**/*.scss", ["compass"]);
 });
 
 gulp.task("default", ["server", "watch"]);
